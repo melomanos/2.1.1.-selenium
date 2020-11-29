47ca1d4d5a5bfcb6cf226f6352a8277addbbf7c2
melomanos <philippov_mihail@mail.ru>
2020-11-29 22:56:12 +0300

    First task done

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,50 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Users\\Meloman\\AppData\\Local\\Google\\Chrome SxS\\Application\\chrome.exe");
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        WebElement elements = driver.findElement(By.cssSelector("form"));
+        elements.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
+        elements.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        elements.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        elements.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
+
